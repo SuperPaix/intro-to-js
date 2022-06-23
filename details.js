3e5d5e49688e81ed9f48bce23822dd55e507abd3
Super_Paix <yawebelle@gmail.com>
2022-06-23 23:54:20 +0100

    first commit

---
+++ details.js
@@ -0,0 +1,7 @@
+let person = {
+    name : "Peace",
+    height : 5.8,
+    country : "Nigeria",
+}
+
+alert('My name is '+ person.name +'.\nI am '+ person.height + ' feet tall, and I am from ' + person.country +'.');
